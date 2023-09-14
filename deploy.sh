89c002e8bb50f189d9900f1cbcbfee5f25fd0623
Ifeoluwa Adeyemi <aifeoluwadiadem@gmail.com>
2023-09-14 15:15:54 +0100

    fix deployment

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t diadem27/multi-client:latest  -f ./client/Dockerfile ./client
-docker build -t diadem27/multi-server:latest  -f ./server/Dockerfile ./server
-docker build -t diadem27/multi-workerlatest -f ./worker/Dockerfile ./worker
+docker build -t diadem27/multi-client:latest -t diadem27/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t diadem27/multi-server:latest -t diadem27/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t diadem27/multi-workerlatest -t diadem27/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push diadem27/multi-client:latest
 docker push diadem27/multi-server:latest
 docker push diadem27/multi-worker:latest
-# docker push diadem27/multi-client:$SHA
-# docker push diadem27/multi-server:$SHA
-# docker push diadem27/multi-worker:$SHA
+docker push diadem27/multi-client:$SHA
+docker push diadem27/multi-server:$SHA
+docker push diadem27/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=diadem27/multi-server
-kubectl set image deployments/client-deployment client=diadem27/multi-client
-kubectl set image deployments/worker-deployment worker=diadem27/multi-worker
+kubectl set image deployments/server-deployment server=diadem27/multi-server:$SHA
+kubectl set image deployments/client-deployment client=diadem27/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=diadem27/multi-worker:$SHA

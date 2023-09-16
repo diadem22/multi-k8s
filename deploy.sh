451698c174fadb0555a76a8bbf8c5e612d9c6138
Ifeoluwa Adeyemi <aifeoluwadiadem@gmail.com>
2023-09-16 21:54:25 +0100

    update certificate.yaml

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t diadem27/multi-client:latest -t diadem27/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t diadem27/multi-server:latest -t diadem27/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t diadem27/multi-workerlatest -t diadem27/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t diadem27/multi-worker:latest -t diadem27/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push diadem27/multi-client:latest
 docker push diadem27/multi-server:latest
 docker push diadem27/multi-worker:latest
